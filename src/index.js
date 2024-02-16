eff8dc4e843b14c54f3fceec4d9df5679efdde3b
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-02-16 15:53:10 +0000

    Updated Weather Data using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 function updateWeather(response) {
   let currtentTemperature = document.querySelector("#weather-app-temperature");
   let cityElement = document.querySelector("#weather-app-city");
+  let currentCondition = document.querySelector("#weather-condition");
+  let currentHumidity = document.querySelector("#humidity");
+  let currentWindSpeed = document.querySelector("#wind-speed");
+  let currentTime = document.querySelector("#weather-app-time");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
   currtentTemperature.innerHTML = Math.round(response.data.temperature.current);
+  currentCondition.innerHTML = response.data.condition.description;
+  currentHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+  currentWindSpeed.innerHTML = `${response.data.wind.speed}Km/h`;
+  currentTime.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  return `${day}, ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -20,4 +55,4 @@ function submitSearch(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
 
-searchCity("Lisbon");
+searchCity("Viseu");
