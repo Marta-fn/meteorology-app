9ba59419864ba2eb3aa19100a4dbdb364e8db47d
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-02-19 15:23:04 +0000

    Added API for the forecast

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function updateWeather(response) {
   currentWindSpeed.innerHTML = `${response.data.wind.speed}Km/h`;
   currentTime.innerHTML = formatDate(date);
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="weather icon">`;
+
+  displayForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -46,7 +48,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "8066f7209f55a447d5b43ta1a1a01obb";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit="metric"`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
   axios.get(apiUrl).then(updateWeather);
 }
 
@@ -56,10 +58,14 @@ function submitSearch(event) {
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", submitSearch);
+function getForecast(city) {
+  let apiKey = "8066f7209f55a447d5b43ta1a1a01obb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
   let forecastHtml = "";
 
@@ -85,5 +91,7 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHtml;
 }
 
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitSearch);
+
 searchCity("Viseu");
-displayForecast();
